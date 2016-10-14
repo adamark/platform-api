6077502d45066264bf3c5b75c278728be86c5bfb
Adam Mark <adam.w.mark@gmail.com>
2016-10-14 14:34:33 -0400

    fix auto-generated syntax issue

--- lib/platform-api/client.rb
+++ lib/platform-api/client.rb
@@ -2349,8 +2349,8 @@ module PlatformAPI
     # Create URLs for uploading and downloading source. Deprecated in favor of `POST /sources`
     #
     # @param app_id_or_app_name: unique identifier of app or unique name of app
-    def create_(deprecated)(app_id_or_app_name)
-      @client.source.create_(deprecated)(app_id_or_app_name)
+    def create_(app_id_or_app_name)
+      @client.source.create_(app_id_or_app_name)
     end
   end
 
